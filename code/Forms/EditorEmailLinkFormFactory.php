9ebe7f84e0d30f003500486b6188ac20db98e82e
Steve Boyd <emteknetnz@gmail.com>
2024-12-05 17:29:03 +1300

    API Rename validator classes

--- code/Forms/EditorEmailLinkFormFactory.php
+++ code/Forms/EditorEmailLinkFormFactory.php
@@ -5,7 +5,7 @@ namespace SilverStripe\Admin\Forms;
 use SilverStripe\Forms\EmailField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
-use SilverStripe\Forms\RequiredFields;
+use SilverStripe\Forms\Validation\RequiredFieldsValidator;
 
 class EditorEmailLinkFormFactory extends LinkFormFactory
 {
@@ -38,7 +38,7 @@ class EditorEmailLinkFormFactory extends LinkFormFactory
     protected function getValidator($controller, $name, $context)
     {
         if ($context['RequireLinkText']) {
-            return RequiredFields::create('Text');
+            return RequiredFieldsValidator::create('Text');
         }
 
         return null;
